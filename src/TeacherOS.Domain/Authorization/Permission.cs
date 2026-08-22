d3d8d1368161529048eb6cfca0eff8dbc53a240e
Ahmed Ragab Saad <ahmedragabsaad7732@gmail.com>
2026-08-22 21:05:03 +0300

    Identify permissions and create Role class

---
+++ src/TeacherOS.Domain/Authorization/Permission.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeacherOS.Domain.Authorization;
+
+public static class Permission
+{
+    public const string AttendanceRecord = "attendance.record";
+    public const string PaymentRecord = "payment.record";
+    public const string PaymentAdjust = "payment.adjust";
+    public const string SessionClose = "session.close";
+    public const string ShiftClose = "shift.close";
+    public const string ContentPublish = "content.publish";
+
+    public static readonly IReadOnlyCollection<string> All =
+    [
+        AttendanceRecord,
+        PaymentRecord,
+        PaymentAdjust,
+        SessionClose,
+        ShiftClose,
+        ContentPublish,
+    ];
+}
